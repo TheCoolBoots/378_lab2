6bc8e698d5f2def439bef70a6a64576dc866ccbf
Andrew Lai <arjlai221@gmail.com>
2022-01-28 11:54:20 -0800

    zombie pathing complete

--- Animated.java
+++ Animated.java
@@ -1,4 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.GreenfootSound;
 
 /**
  * Write a description of class Animated here.
@@ -8,10 +9,21 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Animated extends Actor
 {
-    /**
-     * Act - do whatever the Animated wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    protected GreenfootSound spawnSound;
+    protected GreenfootSound deathSound;
+    
+    protected String rootImgFP;
+    protected int numFrames;
+    protected int moveSpeed;
+    
+    public Animated(){
+                
+    }
+    
+    public void setDirection(int direction){
+        
+    }
+    
     public void act()
     {
         // Add your action code here.
--- Zombie.java
+++ Zombie.java
@@ -1,33 +1,65 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Zombie here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Zombie extends Animated
 {
     private static Player playerRef;
     private static Target targetRef;
     
-    private final String spawnSoundFP = "ZombieSpawn.wav";
-    private final String deathSoundFP = "ZombieDeath.wav";
-    
-    private final String rootImgPath = "Zombie";
-    private final String numFrames = 5;
-    private final String zombieSpeed = 1;
-    
     public Zombie(int xpos, int ypos, Player player, Target target){
         super();
+        this.spawnSound = new GreenfootSound("ZombieSpawn.wav");
+        this.deathSound = new GreenfootSound("ZombieDeath.wav");
+        this.rootImgFP = "Zombie";
+        this.numFrames = 5;
+        this.moveSpeed = 1;
         this.playerRef = player;
         this.targetRef = target;
     }
     
-    public 
+    private double getDistToActor(Actor actor){
+        return Math.sqrt(Math.pow(actor.getX()-this.getX(), 2.0) + Math.pow(actor.getY()-this.getY(), 2));
+    }
+    
+    private int getTargetDirection(){
+        double playerDist = getDistToActor(playerRef);
+        double targetDist = getDistToActor(targetRef);
+        
+        int xDist;
+        int yDist;
+        
+        if(targetDist < playerDist){
+            // move towards target
+            xDist = this.getX() - targetRef.getX();
+            yDist = this.getY() - targetRef.getY();
+        }
+        else{
+            // move towards player
+            xDist = this.getX() - playerRef.getX();
+            yDist = this.getY() - playerRef.getY();
+        }
+        
+        if(Math.abs(xDist) > Math.abs(yDist)){
+            // zombie will be moving up or down
+            if(yDist > 0){
+                return 0;
+            }
+            else{
+                return 2;
+            }
+        }
+        else{
+            // zombie will be moving left or right
+            if(xDist > 0){
+                return 4;
+            }
+            else{
+                return 1;
+            }
+        }
+    }
     
     public void act()
     {
-        // Add your action code here.
+        this.setDirection(this.getTargetDirection());
     }
 }
